05462ed590d881c294fa0e03b7f20f1212a56408
konard <konard@users.noreply.github.com>
2026-04-14 13:22:08 +0000

    deploy: 3725de0bbe3c1eec044c679012df2d26159e9aea

--- rust/trait.impl/core/error/trait.Error.js
+++ rust/trait.impl/core/error/trait.Error.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["platform_data",[["impl&lt;'a, T: <a class=\"trait\" href=\"platform_data/trait.LinkType.html\" title=\"trait platform_data::LinkType\">LinkType</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"platform_data/enum.Error.html\" title=\"enum platform_data::Error\">Error</a>&lt;'a, T&gt;<div class=\"where\">where\n    Self: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a>,</div>",0]]]]);
+    const implementors = Object.fromEntries([["platform_data",[["impl&lt;'a, T: <a class=\"trait\" href=\"platform_data/trait.LinkReference.html\" title=\"trait platform_data::LinkReference\">LinkReference</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"platform_data/enum.Error.html\" title=\"enum platform_data::Error\">Error</a>&lt;'a, T&gt;<div class=\"where\">where\n    Self: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a>,</div>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[725]}
+//{"start":59,"fragment_lengths":[740]}
--- rust/trait.impl/core/fmt/trait.Display.js
+++ rust/trait.impl/core/fmt/trait.Display.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["platform_data",[["impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"platform_data/enum.Error.html\" title=\"enum platform_data::Error\">Error</a>&lt;'a, T&gt;<div class=\"where\">where\n    T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> + <a class=\"trait\" href=\"platform_data/trait.LinkType.html\" title=\"trait platform_data::LinkType\">LinkType</a>,\n    <a class=\"enum\" href=\"https://doc.rust-lang.org/1.94.1/alloc/borrow/enum.Cow.html\" title=\"enum alloc::borrow::Cow\">Cow</a>&lt;'a, T&gt;: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a>,</div>",0]]]]);
+    const implementors = Object.fromEntries([["platform_data",[["impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"platform_data/enum.Error.html\" title=\"enum platform_data::Error\">Error</a>&lt;'a, T&gt;<div class=\"where\">where\n    T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> + <a class=\"trait\" href=\"platform_data/trait.LinkReference.html\" title=\"trait platform_data::LinkReference\">LinkReference</a>,\n    <a class=\"enum\" href=\"https://doc.rust-lang.org/1.94.1/alloc/borrow/enum.Cow.html\" title=\"enum alloc::borrow::Cow\">Cow</a>&lt;'a, T&gt;: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a>,</div>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[878]}
+//{"start":59,"fragment_lengths":[893]}
